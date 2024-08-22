5212321ba215d029f287f441dd86150e2544768a
Hammy <72644858+MumblesIdeas@users.noreply.github.com>
2024-08-21 23:34:30 -0700

    comments

--- LCD DHT11 Testing/basic_LCD_DTH_code/basic_LCD_DTH_code.ino
+++ LCD DHT11 Testing/basic_LCD_DTH_code/basic_LCD_DTH_code.ino
@@ -14,11 +14,13 @@ int DHT11_PIN = 8;
 
 dht DHT;
 // const int RS = 2, EN = 3, D4 = 4, D5 = 5, D6 = 6, D7 = 7;
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // These are the pins on the arduino
+// LiquidCrystal lcd(RS, EN, D4, D5, D6, D7)
+// These are the pins on the arduino
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7); 
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16,2);    //set 16 columns and 2 rows of 16x2 LCD
+  lcd.begin(16,2);    //set 16 columns and 2 rows for the 16x2 LCD
 }
 
 void loop() {
